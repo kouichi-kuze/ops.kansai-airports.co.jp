4a09b55b946b962dbfbaf15c8a1ecd8506b6cfbc
Sayuri Habuka <littlelilly.sky@gmail.com>
2025-05-15 17:18:32 +0900

    header/footerのjsを追加

--- app/public/wp-content/themes/ops_kansai-airports/assets/js/common.js
+++ app/public/wp-content/themes/ops_kansai-airports/assets/js/common.js
@@ -1,4 +1,34 @@
+
 document.addEventListener('DOMContentLoaded', () => {
+// sp nav
+	$(function(){
+		$(".menu-trigger").on("click", function(){
+			if ($("#header-sm").hasClass("open")) {
+			$("#header-sm").removeClass("open");
+			} else {
+			$("#header-sm").addClass("open");
+			}
+		});
+	});
+
+	//sp nav アコーディオン
+	$(".menu-close").each(function () {
+		$(this).on("click", function () {
+			$(this).toggleClass("open");
+			$(this).find(".submenu").slideToggle();
+			return false;
+		});
+	});
+	
+	
+	
 	
 });
+
+
+
+
+
+
+
 
