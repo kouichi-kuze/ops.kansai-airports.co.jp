86384279e397762f486abac561ead77458c3e9fa
Sayuri Habuka <littlelilly.sky@gmail.com>
2025-05-15 17:22:43 +0900

    footer/header/代表メッセージページ作成

--- app/public/wp-content/themes/ops_kansai-airports/inc/footer.php
+++ app/public/wp-content/themes/ops_kansai-airports/inc/footer.php
@@ -1,11 +1,154 @@
 
 <footer class="site-footer">
-フッター
+	<!--sp　footer-->
+	<div id="footer-sm">
+		<div class="footer-bg">
+		  <nav class="nav-sp-menu">
+			<ul>
+			  <li class=""> <a class="menu__item__link js-menu__item__link" href="<?php echo home_url( '/' ); ?>">トップページ</a> </li>
+			  <li class="menu-close"> <a class="menu__item__link js-menu__item__link" href="">企業情報 <span>Company</span></a>
+				<ul class="submenu">
+				  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">代表メッセージ</a> </li>
+				  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">会社概要</a> </li>
+				</ul>
+			  </li>
+			  <li class="menu-close"> <a class="menu__item__link js-menu__item__link" href="">お知らせ <span>Information</span></a>
+				<ul class="submenu">
+				  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+				  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+				  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+				</ul>
+			  </li>
+			  <li class="menu-close"> <a class="menu__item__link js-menu__item__link" href="">事業紹介 <span>Service</span></a>
+				<ul class="submenu">
+				  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+				  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+				  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+				</ul>
+			  </li>
+			  <li class="menu-close"> <a class="menu__item__link js-menu__item__link" href="">採用情報 <span>Recruit</span></a>
+				<ul class="submenu">
+				  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+				  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+				  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+				</ul>
+			  </li>
+			  <li class="menu-close"> <a class="menu__item__link js-menu__item__link" href="">お問い合わせ <span>Contact</span></a>
+				<ul class="submenu">
+				  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+				  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+				  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+				</ul>
+			  </li>
+			</ul>
+		  </nav>
+			<div class="content-inner">
+				<div class="footer-pc-nav-area">
+					  <nav class="nav-pc-menu">
+						<ul>
+						  <li class="fismenu icon_menu_arrow"> <a class="menu__item__link js-menu__item__link" href="<?php echo home_url( '/' ); ?>">トップページ<span>Top</span></a> </li>
+						  <li class="fismenu menu-close"> <a class="menu__item__link js-menu__item__link" href="">企業情報 <span>Company</span></a>
+							<ul class="submenu">
+							  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">代表メッセージ</a> </li>
+							  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">会社概要</a> </li>
+							</ul>
+						  </li>
+						  <li class="fismenu menu-close icon_menu_arrow"> <a class="menu__item__link js-menu__item__link" href="">お知らせ <span>Information</span></a>
+							<ul class="submenu">
+							  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+							  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+							  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+							</ul>
+						  </li>
+						</ul>
+						<ul>
+						  <li class="fismenu menu-close icon_menu_arrow"> <a class="menu__item__link js-menu__item__link" href="">事業紹介 <span>Service</span></a>
+							<ul class="submenu">
+							  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">オペレーションマネジメント事業</a> </li>
+							  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">警備事業</a> </li>
+							  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">消防事業</a> </li>
+							  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">ターミナルマネジメント事業</a> </li>
+							  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">清掃・カート事業</a> </li>
+							  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">駐車場運営事業</a> </li>
+							  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">カーゴエリアオペレーション事業</a> </li>
+							  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">給油運用事業</a> </li>
+							</ul>
+						  </li>
+						</ul>
+						<ul>
+						  <li class="fismenu menu-close icon_menu_arrow"> <a class="menu__item__link js-menu__item__link" href="">採用情報 <span>Recruit</span></a>
+							<ul class="submenu">
+							  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">先輩の声</a> </li>
+							</ul>
+						  </li>
+						  <li class="fismenu menu-close icon_menu_arrow"> <a class="menu__item__link js-menu__item__link" href="">お問い合わせ <span>Contact</span></a>
+<!--
+							<ul class="submenu">
+							  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+							  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+							  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+							</ul>
+-->
+						  </li>
+						</ul>
+					  </nav>
+		
+					
+					
+					<div class="office-info d-md-flex justify-content-md-between">
+						
+						<div>
+							<div class="logo-l"><a href="<?php echo home_url( '/' ); ?>"><img src="<?php bloginfo('template_url'); ?>/assets/img/common/logo_l.svg" alt="関西エアポートエアポートオペレーションサービス株式会社"></a></div>							
+							<ul class="office-list">
+								<li class="offis-item">
+									<p class="office-name">関西エアポートオペレーションサービス株式会社</p>
+									<p class="office-acssec">〒549-0001<br>大阪府泉佐野市泉州空港北1番地<br>関西国際空港航空会社ビル4階<span class="map"><a class="map-link" href="<?php echo home_url( '/' ); ?>">MAP</a></span></p>
+								</li>
+								<li class="offis-item">
+									<p class="office-name">関西エアポートオペレーションサービス株式会社<br>伊丹オフィス</p>
+									<p class="office-acssec">〒560-0036<br>大阪府豊中市蛍池西町3丁目555番地<span class="map"><a class="map-link" href="<?php echo home_url( '/' ); ?>">MAP</a></span></p>
+								</li>
+							</ul>
+						</div>
+						
+						
+						<div class="footer-btn"><a href="<?php echo home_url( '/' ); ?>"><img src="<?php bloginfo('template_url'); ?>/assets/img/common/logo_en.svg" alt="関西エアポートエアポートオペレーションサービス株式会社"></a><span>関西エアポート株式会社</span></div>
+					</div>
+				</div>
+				
+			</div>
+			<div class="content-inner">
+				<div class="footer-copylink-area">
+					<ul class="footer-link-list">
+						<li class="footer-link-item"><a href="<?php echo home_url( '/' ); ?>">クッキーポリシー</a></li>
+						<li class="footer-link-item"><a href="<?php echo home_url( '/' ); ?>">個人情報保護方針</a></li>
+						<li class="footer-link-item icon_privacypolicy"><a href="<?php echo home_url( '/' ); ?>"><img src="<?php bloginfo('template_url'); ?>/assets/img/common/icon_privacypolicy.png" alt="個人情報保護方針"></a></li>
+					</ul>
+					<div class="copyright">© Kansai Airports Operation Services</div>
+				</div>	
+			</div>
+	
+		</div>
+	</div>
+	
+
+
+
+
+
+
 </footer>
 </div>
 <!-- /wrapper -->
+<!-- jQuery CDN --> 
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<!-- 自作JS --> 
+<script type="<?php echo get_stylesheet_directory_uri() ?>/assets/js/common.js"></script>
 
 <?php get_template_part( 'inc/script' ); ?>
+
+
+
 <?php wp_footer(); ?>
 </body>
 </html>
--- app/public/wp-content/themes/ops_kansai-airports/inc/header.php
+++ app/public/wp-content/themes/ops_kansai-airports/inc/header.php
@@ -9,11 +9,137 @@
 <?php get_template_part( 'inc/head' ); ?>
 <?php wp_head(); ?>
 <?php get_template_part( 'inc/css' ); ?>
+<link href="<?php echo get_stylesheet_directory_uri() ?>/assets/css/style.css" rel="stylesheet">
 </head>
 <body>
 <?php get_template_part( 'inc/g_tag_body' ); ?>
 <!-- wrapper -->
 <div id="wrapper">
 <header class="site-header">
-<img src="<?php bloginfo('template_url'); ?>/assets/img/common/logo.svg" alt="関西エアポートオペレーションサービス株式会社">
+<!-- <img src="<?php bloginfo('template_url'); ?>/assets/img/common/logo.svg" alt="関西エアポートオペレーションサービス株式会社"> -->
+
+	<!--pc　headerー-->
+	<div  id="header-md" >
+		<div class="header-bg">
+			<h1 class="s-header-logo"> <a href="<?php echo home_url( '/' ); ?>"><img src="<?php bloginfo('template_url'); ?>/assets/img/common/logo.svg" alt="関西エアポートエアポートオペレーションサービス株式会社"></a> </h1>
+
+			<nav class="nav-pc-menu">
+			<ul class="nav-list">
+			  <li class="nav-item"> 
+				  <a class="nav-iink" href="">企業情報 <span>Company</span></a>
+				  <div class="secondmenu_outline">
+					<ul class="secondmenu">
+					  <li class="secondmenu-top_item"> <a class="" href="<?php echo home_url( '/' ); ?>">企業情報トップ</a> </li>
+					  <li class="secondmenu_item"> <a class="" href="<?php echo home_url( '/' ); ?>">代表メッセージ</a> </li>
+					  <li class="secondmenu_item"> <a class="" href="<?php echo home_url( '/' ); ?>">会社概要</a> </li>
+					</ul>
+				  </div>
+			  </li>
+			  <li class="nav-item"> 
+				  <a class="nav-iink" href="">お知らせ <span>Information</span></a>
+				  <div class="secondmenu_outline">
+					<ul class="secondmenu">
+					  <li class="secondmenu-top_item"> <a class="" href="<?php echo home_url( '/' ); ?>">お知らせトップ</a> </li>
+					  <li class="secondmenu_item"> <a class="" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+					  <li class="secondmenu_item"> <a class="" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+					  <li class="secondmenu_item"> <a class="" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+					</ul>
+				  </div>
+			  </li>
+			  <li class="nav-item"> 
+				  <a class="nav-iink" href="">事業紹介 <span>Service</span></a>
+				  <div class="secondmenu_outline">
+					<ul class="secondmenu">
+					  <li class="secondmenu-top_item"> <a class="" href="<?php echo home_url( '/' ); ?>">事業紹介トップ</a> </li>
+					  <li class="secondmenu_item"> <a class="" href="<?php echo home_url( '/' ); ?>">警備事業</a> </li>
+					  <li class="secondmenu_item"> <a class="" href="<?php echo home_url( '/' ); ?>">消防事業</a> </li>
+					  <li class="secondmenu_item"> <a class="" href="<?php echo home_url( '/' ); ?>">カーゴエリアオペレーション事業</a> </li>
+					  <li class="secondmenu_item"> <a class="" href="<?php echo home_url( '/' ); ?>">清掃・カート事業</a> </li>
+					  <li class="secondmenu_item"> <a class="" href="<?php echo home_url( '/' ); ?>">ターミナルマネジメント事業</a> </li>
+					  <li class="secondmenu_item"> <a class="" href="<?php echo home_url( '/' ); ?>">駐車場運営事業</a> </li>
+					  <li class="secondmenu_item"> <a class="" href="<?php echo home_url( '/' ); ?>">給油運用事業</a> </li>
+					</ul>
+				  </div>
+			  </li>
+			  <li class="nav-item"> 
+				  <a class="nav-iink" href="">採用情報 <span>Recruit</span></a>
+				  <div class="secondmenu_outline">
+					<ul class="secondmenu">
+					  <li class="secondmenu-top_item"> <a class="" href="<?php echo home_url( '/' ); ?>">採用情報トップ</a> </li>
+					  <li class="secondmenu_item"> <a class="" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+					  <li class="secondmenu_item"> <a class="" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+					  <li class="secondmenu_item"> <a class="" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+					</ul>
+				  </div>
+			  </li>
+			  <li class="nav-item"> 
+				  <a class="nav-iink" href="">お問い合わせ <span>Contact</span></a>
+				  <div class="secondmenu_outline righa-adj">
+					<ul class="secondmenu">
+					  <li class="secondmenu-top_item"> <a class="" href="<?php echo home_url( '/' ); ?>">お問い合わせトップ</a> </li>
+					  <li class="secondmenu_item"> <a class="" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+					  <li class="secondmenu_item"> <a class="" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+					  <li class="secondmenu_item"> <a class="" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+					</ul>
+				  </div>
+			  </li>
+			</ul>
+		  </nav>
+
+		</div>
+	</div>
+	<!--sp　header-->
+	<div id="header-sm" class="d-xd-none">
+	  <div class="header-flex">
+		<h1 class="s-header-logo"> <a href="<?php echo home_url( '/' ); ?>"><img src="<?php bloginfo('template_url'); ?>/assets/img/common/logo.svg" alt="関西エアポートエアポートオペレーションサービス株式会社"></a> </h1>
+		<div class="menu-trigger menu-bg" href="">
+		  <div class="menu-position"> <span></span> <span></span> <span></span> </div>
+		</div>
+	  </div>
+	  <nav class="nav-sp-menu">
+		<ul>
+		  <li class=""> <a class="menu__item__link js-menu__item__link" href="<?php echo home_url( '/' ); ?>">トップページ</a> </li>
+		  <li class="menu-close"> <a class="menu__item__link js-menu__item__link" href="">企業情報 <span>Company</span></a>
+			<ul class="submenu">
+			  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">代表メッセージ</a> </li>
+			  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">会社概要</a> </li>
+			</ul>
+		  </li>
+		  <li class="menu-close"> <a class="menu__item__link js-menu__item__link" href="">お知らせ <span>Information</span></a>
+			<ul class="submenu">
+			  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+			  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+			  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+			</ul>
+		  </li>
+		  <li class="menu-close"> <a class="menu__item__link js-menu__item__link" href="">事業紹介 <span>Service</span></a>
+			<ul class="submenu">
+			  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+			  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+			  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+			</ul>
+		  </li>
+		  <li class="menu-close"> <a class="menu__item__link js-menu__item__link" href="">採用情報 <span>Recruit</span></a>
+			<ul class="submenu">
+			  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+			  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+			  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+			</ul>
+		  </li>
+		  <li class="menu-close"> <a class="menu__item__link js-menu__item__link" href="">お問い合わせ <span>Contact</span></a>
+			<ul class="submenu">
+			  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+			  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+			  <li class="submenu__item"> <a class="nav-close-menu" href="<?php echo home_url( '/' ); ?>">サブメニュー</a> </li>
+			</ul>
+		  </li>
+		</ul>
+	  </nav>
+	</div>
+
+
+
+
+
+
 </header>
