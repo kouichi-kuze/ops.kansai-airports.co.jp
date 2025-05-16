29d1a6c015e9e23b953f4ab8b25ff2bdcc1d81c6
Sayuri Habuka <littlelilly.sky@gmail.com>
2025-05-17 02:47:56 +0900

    js追加　headerの不具合修正　企業概要のjs作成　スムーススクロール作成

--- app/public/wp-content/themes/ops_kansai-airports/assets/js/common.js
+++ app/public/wp-content/themes/ops_kansai-airports/assets/js/common.js
@@ -1,6 +1,6 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-// sp nav
+	// sp nav
 	$(function(){
 		$(".menu-trigger").on("click", function(){
 			if ($("#header-sm").hasClass("open")) {
@@ -12,19 +12,71 @@ document.addEventListener('DOMContentLoaded', () => {
 	});
 
 	//sp nav アコーディオン
-	$(".menu-close").each(function () {
+	$(".js-menu__item__link").each(function () {
 		$(this).on("click", function () {
 			$(this).toggleClass("open");
-			$(this).find(".submenu").slideToggle();
+			$(this).next(".submenu").slideToggle();
 			return false;
 		});
 	});
+	
+	//ページ内スクロール
+	$(function(){
+		$('a[href^="#"]').click(function(){
+			var adjust = 0;
+			var speed = 400;
+			var href= $(this).attr("href");
+			var target = $(href == "#" || href == "" ? 'html' : href);
+			var position = target.offset().top + adjust;
+			$('body,html').animate({scrollTop:position}, speed, 'swing');
+			return false;
+		});
+	});
+	
+	//スクロール後非表示
+	$(function () {
+		const $list = $('.page-content-link-list');
+		const $end = $('.hidden-area');
+
+		$(window).on('scroll', function () {
+				const scrollTop = $(this).scrollTop();
+				const windowBottom = scrollTop + $(window).height();
+
+			// fixed クラスの付け外し（300px以上スクロールしたら）
+			if (scrollTop >= 300) {
+				$list.addClass('fixed');
+				} else {
+				$list.removeClass('fixed');
+			}
+
+			// hidden-area に差し掛かったら非表示
+			if ($list.length && $end.length) {
+				const endTop = $end.offset().top;
+
+				if (windowBottom >= endTop) {
+				$list.fadeOut();
+				} else {
+				$list.fadeIn();
+				}
+			}
+		});
+	});	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
 	
 });
-
 
 
 
