8e6dc559d2ee3e4b1680145e354a5ed538b21020
Sayuri Habuka <littlelilly.sky@gmail.com>
2025-05-17 03:12:28 +0900

    会社概要ページ作成

--- app/public/wp-content/themes/ops_kansai-airports/template/page-outline.php
+++ app/public/wp-content/themes/ops_kansai-airports/template/page-outline.php
@@ -19,6 +19,283 @@ Template Name:企業概要
   </div>
 </div>
 <main  id="page-company-outline">
+	
+	
+	
+	
+		<div class="sub-page-main bg-lightpurple">
+			<div class="p-side-3-8">
+				<div class="left-content">
+					<div class="read">
+						<p class="read-ttl"><img src="<?php bloginfo('template_url'); ?>//assets/img/outline/outline_read_ttl.png" alt="空港の安全　安心を守る"></p>
+						<p class="text　fixed-area">関西エアポートオペレーションサービス株式会社は、関西エアポート株式会社グループとして、様々な空港運用に関するオペレーション業務で関西国際空港、大阪国際空港の高品質なオペレーションサービスを提供しています。</p>
+					</div>
+					<ul class="page-content-link-list">
+						<li class="page-content-link"><a href="#page-content-link_01">消火器の設置・法定点検等</a></li>
+						<li class="page-content-link"><a href="#page-content-link_02">組織体制</a></li>
+						<li class="page-content-link"><a href="#page-content-link_03">所在地</a></li>
+					</ul>
+					<div id="page-content-link_01" class="content">
+						<div class="margin-adjust">
+							<h2 class="left-pinkline-ttl">消火器の設置・法定点検等</h2>
+						</div>
+						<div class="company-profile">
+							<dl class="company-profile-item">
+								<dt class="title">会社名</dt>
+								<dd class="text">関西エアポートオペレーションサービス株式会社</dd>
+							</dl>
+							<dl class="company-profile-item">
+								<dt class="title">所在地</dt>
+								<dd class="text">〒549-0001　大阪府泉佐野市泉州空港北1番地　関西国際空港航空会社ビル4階</dd>
+							</dl>
+							<dl class="company-profile-item">
+								<dt class="title">設立</dt>
+								<dd class="text">1991年（平成3年）7月17日</dd>
+							</dl>
+							<dl class="company-profile-item">
+								<dt class="title">資本金</dt>
+								<dd class="text">2,000万円</dd>
+							</dl>
+							<dl class="company-profile-item">
+								<dt class="title">出資者</dt>
+								<dd class="text">関西エアポート株式会社（全額出資）</dd>
+							</dl>
+							<dl class="company-profile-item">
+								<dt class="title">代表者</dt>
+								<dd class="text">
+									代表取締役会長：ヤニック・アイユリ<br>
+									代表取締役社長：石川 浩司<br>
+									取締役：北林 弘幹<br>
+									監査役：坂本 龍平<br>
+									監査役：片平　聡<br>
+									監査役：坂本 千佳
+								</dd>
+							</dl>
+							<dl class="company-profile-item">
+								<dt class="title">事業内容</dt>
+								<dd class="text">
+									<ol class="business-list">
+										<li class="business-item">空港における消防及び防災業務</li>
+										<li class="business-item">警備業法（昭和47年、法律第117号）第2条第1項に定める警備業務</li>
+										<li class="business-item">空港利用者に対する案内業務及び旅客サービス提供業務</li>
+										<li class="business-item">空港における駐車場に関する業務</li>
+										<li class="business-item">空港における施設の運用監視、日常運用に関する業務</li>
+										<li class="business-item">空港における航空機給油施設の運営及び維持管理に関する業務</li>
+										<li class="business-item">空港における施設の清掃に関する業務</li>
+										<li class="business-item">不動産賃貸に関する業務</li>
+										<li class="business-item">前各号に付帯する一切の業務</li>
+									</ol>
+								</dd>
+							</dl>
+							<dl class="company-profile-item">
+								<dt class="title">従業員数</dt>
+								<dd class="text">
+									841名(2024年4月1日現在)
+									<div class="blank-hiringratio-head">
+										<h3 class="dot-ttl">労働施策総合推進法に基づく中途採用比率の公開</h3>
+										<p class="date">公表日：2025年4月1日</p>
+									</div>
+									<div class="blank-hiringratio-table">
+										<dl class="blank-hiringratio-item  fiscal-year">
+											<dt class="title">年度</dt>
+											<dd class="text">2022年度</dd>
+											<dd class="text">2023年度</dd>
+											<dd class="text">2024年度</dd>
+										</dl>
+										<dl class="blank-hiringratio-item ratio">
+											<dt class="title">正規雇用労働者<br class="d-sm-none"> の中途採用比率*</dt>
+											<dd class="text">88%</dd>
+											<dd class="text">93%</dd>
+											<dd class="text">94%</dd>
+										</dl>
+									</div>
+									<p class="text-right attention">*正規雇用労働者の採用数に占める中途採用者比率</p>
+								
+								</dd>
+							</dl>
+						</div>
+					</div>
+				</div>
+			</div>
+			<section class="sec-bulue-box">
+				<div id="page-content-link_02" class="content">
+					<div class="p-side-3-8">
+						<div class="left-content">
+							<div class="margin-adjust">
+								<h2 class="left-pinkline-ttl">組織体制</h2>
+							</div>
+							<div class="company-system">
+								<dl class="company-system-item">
+									<dt class="title">管理部</dt>
+									<dd class="text">
+										<ol class="company-affi-list">
+											<li class="company-affi-item">総務人事グループ</li>
+										</ol>
+									</dd>
+								</dl>
+								<dl class="company-system-item">
+									<dt class="title">財務部</dt>
+									<dd class="text">
+										<ol class="company-affi-list">
+											<li class="company-affi-item">財務グループ</li>
+										</ol>
+									</dd>
+								</dl>
+								<dl class="company-system-item single">
+									<dt class="title">コンプライアンス安全 ・ 担当</dt>
+
+								</dl>
+								<dl class="company-system-item">
+									<dt class="title">オペレーションマネジメント部</dt>
+									<dd class="text">
+										<ol class="company-affi-list">
+											<li class="company-affi-item">管理統括グループ</li>
+											<li class="company-affi-item">ターミナル運用統括グループ</li>
+											<li class="company-affi-item">安全・保安統括グループ</li>
+											<li class="company-affi-item">運用企画統括グループ</li>
+											<li class="company-affi-item">運用企画グループ</li>
+											<li class="company-affi-item">事業開発グループ</li>
+											<li class="company-affi-item">渉外・調査チーム</li>
+										</ol>
+									</dd>
+								</dl>
+								<dl class="company-system-item">
+									<dt class="title">給油施設部</dt>
+									<dd class="text">
+										<ol class="company-affi-list">
+											<li class="company-affi-item">給油管理グループ</li>
+											<li class="company-affi-item">給油保守グループ</li>
+											<li class="company-affi-item">給油運用グループ</li>
+											<li class="company-affi-item">給油管制グループ</li>
+										</ol>
+									</dd>
+								</dl>
+								<dl class="company-system-item">
+									<dt class="title">(KIX ITM)<br>エアフィールドオペレーションセンター</dt>
+									<dd class="text">
+										<ol class="company-affi-list">
+											<li class="company-affi-item">消防隊</li>
+											<li class="company-affi-item">警備隊</li>
+										</ol>
+									</dd>
+								</dl>
+								<dl class="company-system-item">
+									<dt class="title">(KIX ITM)<br>ターミナルエリアオペレーションセンター</dt>
+									<dd class="text">
+										<ol class="company-affi-list">
+											<li class="company-affi-item">ターミナルマネジメントグループ</li>
+											<li class="company-affi-item">パッセンジャーサポートグループ</li>
+											<li class="company-affi-item">駐車場運営グループ</li>
+											<li class="company-affi-item">清掃・カートグループ</li>
+											<li class="company-affi-item">オペレーションサポートグループ</li>
+										</ol>
+									</dd>
+								</dl>
+								<dl class="company-system-item single">
+									<dt class="title">ノースエリアオペレーションセンター</dt>
+								</dl>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section class="sec-box">
+				<div id="page-content-link_03">
+					<div class="p-side-3-8">
+						<div class="left-content">
+							
+							<div class="margin-adjust">
+								<h2 class="left-pinkline-ttl">所在地</h2>
+							</div>
+							<div class="company-location-outer">
+								<div class="maps-group digit visible">
+									<!--iframe-->
+									<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d13200.147926195737!2d135.24371581691418!3d34.4332215720831!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sja!2sus!4v1747384890285!5m2!1sja!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+								</div>
+								<dl class="company-location">
+									<dt class="title">関西エアポートオペレーションサービス株式会社</dt>
+									<dd class="text">
+										<p class="address">〒549-0001<br>大阪府泉佐野市泉州空港北1番地 関西国際空港航空会社北ビル4階</p>
+										<p class="tel">
+											<a href="tel:072-455-4980">TEL : 072-455-4980</a>
+											<a href="tel:072-455-4981">FAX : 072-455-4981</a>
+										</p>
+									</dd>
+									<dd class="contact">
+										<div class="contact-link"><a href="<?php echo home_url( '/' ); ?>">業務内容別のご連絡先はこちら</a></div>
+										<div class="pdf"><a href="<?php echo home_url( '/' ); ?>">鉄道駅からの案内図<img src="<?php bloginfo('template_url'); ?>//assets/img/outline/icon_lightblue_pdf.png" alt="pdfアイコン"></a></div>
+									</dd>
+								</dl>
+								<div class="maps-group digit visible">
+									<!--iframe-->
+									<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d5840.570654992171!2d135.4418710518263!3d34.78954443166112!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sja!2sjp!4v1747385016643!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+								</div>
+								<dl class="company-location">
+									<dt class="title">関西エアポートオペレーションサービス株式会社　伊丹オフィス</dt>
+									<dd class="text">
+										<p class="address">〒560-0036<br>大阪府豊中市蛍池西町3丁目555番地</p>
+										<p class="tel">
+											<a href="tel:06-6856-7437">TEL : 06-6856-7437</a>
+											<a href="tel:06-6845-1037">FAX : 06-6845-1037</a>
+										</p>
+									</dd>
+									<dd class="contact">
+										<div class="pdf"><a href="<?php echo home_url( '/' ); ?>">鉄道駅からの案内図<img src="<?php bloginfo('template_url'); ?>//assets/img/outline/icon_lightblue_pdf.png" alt="pdfアイコン"></a></div>
+									</dd>
+								</dl>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section class="companyinfo-box hidden-area">
+				<div class="companyinfo-content">
+				  <div class="page-header__titles">
+					<h1 class="page-header__title">企業情報</h1>
+					<p class="page-header__subtitle">Company Information</p>
+				  </div>
+					<ul class="companyinfo-list">
+						<li class="companyinfo-item"><a class="companyinfo-link" href="<?php echo home_url( '/' ); ?>">代表メッセージ</a></li>
+						<li class="companyinfo-item"><a class="companyinfo-link" href="<?php echo home_url( '/' ); ?>">会社概要</a></li>
+					</ul>
+				</div>
+			</section>
+		</div>
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 </main >
 <?php get_template_part( 'inc/footer' ); ?>
