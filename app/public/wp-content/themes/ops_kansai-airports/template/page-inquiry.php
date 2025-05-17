2cba57f797cc8aa6421ec5ed5ea5f7ef3a0d5629
Sayuri Habuka <littlelilly.sky@gmail.com>
2025-05-18 03:08:26 +0900

    お問い合わせページ作成

--- app/public/wp-content/themes/ops_kansai-airports/template/page-inquiry.php
+++ app/public/wp-content/themes/ops_kansai-airports/template/page-inquiry.php
@@ -6,7 +6,7 @@ Template Name:お問い合わせ
 <?php get_template_part( 'inc/header' ); ?>
 
 
-<div class="sub-page-header">
+<div class="sub-page-header p-side-3-8">
   <!-- パンくずリスト -->
   <div class="breadcrumb-wrap">
     <?php get_template_part( 'inc/breadcrumb' ); ?>
@@ -18,7 +18,202 @@ Template Name:お問い合わせ
     <p class="page-header__subtitle">Contact</p>
   </div>
 </div>
-<main  id="page-company-inquiry">
 
+
+<main  id="page-company-inquiry">
+	<div class="sub-page-main bg-lightgreen">
+		<div class="p-side-3-16">
+			<div class="">
+				<div class="left-content">
+					<div class="read">
+						<p class="read-ttl"><span class="sub-ttl">営業のご案内</span>「空港での安全を保証し、<br>お客様の信頼にお応えします。」</p>
+
+
+						<p class="text fixed-area">関西エアポートオペレーションサービス株式会社は関西エアポート株式会社グループとして空港の「安全・安心」を守ってまいりました。弊社は長年の経験から培ったノウハウを活かし、お客様のご依頼にお応えします。<br>詳しくは、電話でお問い合わせください。</p>
+					</div>
+					<ul class="page-content-link-list">
+						<li class="page-content-link"><a href="#page-content-link_01">受託警備業務</a></li>
+						<li class="page-content-link"><a href="#page-content-link_02">消火器の設置・<br class="d-xl-inline-block">法定点検等</a></li>
+						<li class="page-content-link"><a href="#page-content-link_03">事業別お問い合わせ先</a></li>
+						<li class="page-content-link"><a href="#page-content-link_04">オフィスクリーニング<br class="d-xl-inline-block">について</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="pl-4-16">
+			<div class="sec-bulue-box-outer">
+				<section class="sec-bulue-box">
+					<div id="page-content-link_01" class="content">
+						<div class="p-side-3-8">
+							<div class="left-content">
+								<div class="security-outer">
+									<div class="margin-adjust">
+										<h2 class="left-pinkline-ttl">受託警備業務</h2>
+									</div>
+									<div class="security">
+										<p class="left-border-ttl">関西国際空港</p>
+										<ol class="disc-list">
+											<li class="disc-item">空港制限区域への出入り管理業務</li>
+											<li class="disc-item">空港内施設の警備業務</li>
+											<li class="disc-item">各種イベント等における警備業務</li>
+											<li class="disc-item">その他警備業務全般</li>
+										</ol>
+										<div class="security-img">
+											<div id="slick-slider" class="slick-slider">
+												<div class="slick-item">
+													<div class="slick-item-inner">
+														<img src="<?php bloginfo('template_url'); ?>/assets/img/inquiry/img_security01.png" alt="警備をしている様子">
+													</div>
+												</div>
+												<div class="slick-item">
+													<div class="slick-item-inner">
+														<img src="<?php bloginfo('template_url'); ?>/assets/img/inquiry/img_security02.png" alt="車から警備の連絡を無線でしている様子">
+													</div>
+												</div>
+											</div>
+										</div>
+										<h3 class="left-bulueline-ttl">ご契約にあたって</h3>
+										<p class="text fixed-area">警備業法第19条に基づき、契約に関する重要事項について、以下のファイル（PDF）に記載しておりますので、よくお読みいただいた上でご契約下さい。 <br>なお、ダウンロードできない場合や書面による説明を希望される方は、電話でお問い合わせ下さい。</p>
+
+										<div class="dot-ttl">契約に関する重要事項</div>
+										<ul class="pdf-list">
+											<li class="pdf-item"><a class="companyinfo-link" href="<?php echo home_url( '/' ); ?>">鉄道駅からの案内図<img src="<?php bloginfo('template_url'); ?>/assets/img/outline/icon_lightblue_pdf.png" alt="pdfアイコン"></a></li>
+											<li class="pdf-item"><a class="companyinfo-link" href="<?php echo home_url( '/' ); ?>">交通誘導警備業務/雑踏警備業務<img src="<?php bloginfo('template_url'); ?>/assets/img/outline/icon_lightblue_pdf.png" alt="pdfアイコン"></a></li>
+											<li class="pdf-item"><a class="companyinfo-link" href="<?php echo home_url( '/' ); ?>">運搬警備業務<img src="<?php bloginfo('template_url'); ?>/assets/img/outline/icon_lightblue_pdf.png" alt="pdfアイコン"></a></li>
+											<li class="pdf-item"><a class="companyinfo-link" href="<?php echo home_url( '/' ); ?>">身辺警備業務<img src="<?php bloginfo('template_url'); ?>/assets/img/outline/icon_lightblue_pdf.png" alt="pdfアイコン"></a></li>
+											<li class="pdf-item"><a class="companyinfo-link" href="<?php echo home_url( '/' ); ?>">機械警備業務<img src="<?php bloginfo('template_url'); ?>/assets/img/outline/icon_lightblue_pdf.png" alt="pdfアイコン"></a></li>
+										</ul>
+										<div class="white-radius-box">
+											<div class="dot-ttl">警備に関するお問い合わせ（担当：企画・管理部　総務人事グループ）</div>
+											<div class="tel"><a href="tel:072-455-2794"><span>Tel.</span>072-455-2794</a></div>
+											<div class="text">受付時間 09:00～17:30／※ 土・日・祝日・年末年始をのぞく</div>
+										</div>
+
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+			</div>
+		</div>
+		<div class="pl-4-16">
+				<section class="sec-bulue-box">
+					<div id="page-content-link_02" class="content">
+						<div class="p-side-3-8">
+							<div class="left-content">
+								<div class="security-outer">
+									<div class="margin-adjust">
+										<h2 class="left-pinkline-ttl">消火器の設置・法定点検等</h2>
+									</div>
+									<div class="security">
+										<p class="left-border-ttl">関西国際空港</p>
+										<ol class="disc-list">
+											<li class="disc-item">消火器の設置・更新・廃棄</li>
+											<li class="disc-item">消火器の法定点検</li>
+											<li class="disc-item">消火器に関する消防署への届出</li>
+										</ol>
+										<h3 class="left-bulueline-ttl">ご契約にあたって</h3>
+										<p class="text fixed-area">ご契約に際しては、以下の契約約款をご確認下さい。</p>
+
+										<div class="dot-ttl">契約に関する重要事項</div>
+										<ul class="pdf-list">
+											<li class="pdf-item"><a class="companyinfo-link" href="<?php echo home_url( '/' ); ?>">消火器法定点検等業務契約約款<img src="<?php bloginfo('template_url'); ?>/assets/img/outline/icon_lightblue_pdf.png" alt="pdfアイコン"></a></li>
+										</ul>
+										<div class="white-radius-box">
+											<div class="dot-ttl">警備に関するお問い合わせ（担当：企画・管理部　総務人事グループ）</div>
+											<div class="tel"><a href="tel:072-455-2794"><span>Tel.</span>072-455-2794</a></div>
+											<div class="text">受付時間 09:00～17:30／※ 土・日・祝日・年末年始をのぞく</div>
+										</div>
+
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+		</div>
+		<div class="pl-4-16">
+				<section class="sec-bulue-box">
+					<div id="page-content-link_03" class="content">
+						<div class="p-side-3-8">
+							<div class="left-content">
+								<div class="security-outer">
+									<div class="margin-adjust">
+										<h2 class="left-pinkline-ttl">事業別お問い合わせ先</h2>
+									</div>
+									<div class="white-radius-box">
+										<div class="dot-ttl">オペレーションマネジメント事業</div>
+										<p class="left-border-ttl">関西国際空港</p>
+										<div class="tel"><a href="tel:072-455-2406"><span>Tel.</span>072-455-2406</a></div>
+										<div class="text">受付時間 09:00～17:30／※ 土・日・祝日・年末年始をのぞく</div>
+									</div>
+									<div class="white-radius-box">
+										<div class="dot-ttl">警備事業</div>
+										<div class="mb-2-4">
+											<p class="left-border-ttl">関西国際空港</p>
+											<div class="tel"><a href="tel:072-455-2421"><span>Tel.</span>072-455-2421</a></div>
+											<div class="text">受付時間 09:00～17:30／※ 土・日・祝日・年末年始をのぞく</div>
+										</div>
+										<div class="mb-2-4">
+											<p class="left-border-ttl">大阪国際空港</p>
+											<div class="tel"><a href="tel:06-6856-7437"><span>Tel.</span>06-6856-7437</a></div>
+											<div class="text">受付時間 09:00～17:30／※ 土・日・祝日・年末年始をのぞく</div>
+										</div>
+									</div>
+									<div class="white-radius-box">
+										<div class="dot-ttl">消防事業</div>
+										<div class="mb-2-4">
+											<p class="left-border-ttl">関西国際空港</p>
+											<div class="tel"><a href="tel:072-455-2404"><span>Tel.</span>072-455-2404</a></div>
+											<div class="text">受付時間 09:00～17:30／※ 土・日・祝日・年末年始をのぞく</div>
+										</div>
+										<div class="mb-2-4">
+											<p class="left-border-ttl">大阪国際空港</p>
+											<div class="tel"><a href="tel:06-4865-9631"><span>Tel.</span>06-4865-9631</a></div>
+											<div class="text">受付時間 09:00～17:30／※ 土・日・祝日・年末年始をのぞく</div>
+										</div>
+									</div>
+									<div class="white-radius-box">
+										<div class="dot-ttl">デイリーメンテナンス事業</div>
+										<div class="mb-2-4">
+											<p class="left-border-ttl">関西国際空港</p>
+											<div class="tel"><a href="tel:072-455-4920"><span>Tel.</span>072-455-4920</a></div>
+											<div class="text">受付時間 09:00～17:30／※ 土・日・祝日・年末年始をのぞく</div>
+										</div>
+										<div class="mb-2-4">
+											<p class="left-border-ttl">大阪国際空港</p>
+											<div class="tel"><a href="tel:06-6856-6768"><span>Tel.</span>06-6856-6768</a></div>
+											<div class="text">受付時間 09:00～17:30／※ 土・日・祝日・年末年始をのぞく</div>
+										</div>
+									</div>
+									<div class="white-radius-box">
+										<div class="dot-ttl">駐車場運営事業</div>
+										<div class="mb-2-4">
+											<p class="left-border-ttl">関西国際空港</p>
+											<div class="tel"><a href="tel:072-455-2336"><span>Tel.</span>072-455-2336</a></div>
+											<div class="text">受付時間 09:00～17:30／※ 土・日・祝日・年末年始をのぞく</div>
+										</div>
+										<div class="mb-2-4">
+											<p class="left-border-ttl">大阪国際空港</p>
+											<div class="tel"><a href="tel:06-6856-7300"><span>Tel.</span>006-6856-7300</a></div>
+											<div class="text">受付時間 09:00～17:30／※ 土・日・祝日・年末年始をのぞく</div>
+										</div>
+									</div>
+
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+		</div>
+			<div id="page-content-link_04" class="bnr_office_cleaning p-side-3-16 hidden-area">
+				<div class="margin-adjust">
+					<a class="pc d-md-block" href="https://www.ops.kansai-airports.co.jp/services/cart-cleaning/cleaning/"><img src="<?php bloginfo('template_url'); ?>/assets/img/inquiry/bnr_office_cleaning_pc.png" alt="オフィスクリーニング　詳しくはこちら"></a>
+					<a class="sp d-sm-none" href="https://www.ops.kansai-airports.co.jp/services/cart-cleaning/cleaning/"><img src="<?php bloginfo('template_url'); ?>/assets/img/inquiry/bnr_office_cleaning_sp.png" alt="オフィスクリーニング　詳しくはこちら"></a>
+				</div>
+			</div>
+	</div>
+	
 </main >
 <?php get_template_part( 'inc/footer' ); ?>
