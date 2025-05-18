19be1c1b6811748c7abb1ecb7be5dd26e74c465e
Sayuri Habuka <littlelilly.sky@gmail.com>
2025-05-18 18:06:49 +0900

    事業紹介ページ作成

--- app/public/wp-content/themes/ops_kansai-airports/template/page-services.php
+++ app/public/wp-content/themes/ops_kansai-airports/template/page-services.php
@@ -6,7 +6,7 @@ Template Name:事業紹介
 <?php get_template_part( 'inc/header' ); ?>
 
 
-<div class="sub-page-header">
+<div class="sub-page-header p-side-3-8">
   <!-- パンくずリスト -->
   <div class="breadcrumb-wrap">
     <?php get_template_part( 'inc/breadcrumb' ); ?>
@@ -18,7 +18,195 @@ Template Name:事業紹介
     <p class="page-header__subtitle">Service</p>
   </div>
 </div>
-<main id="page-company-service">
+<main id="page-company-service" class="company-service bg-color-primary">
+	<div class="main-ounded-content">
+		<div class="p-side-15-8">
+			<div class="max-128">
+				<h2 class="bottom-border-bulue-ttl">関西エアポート<br class="d-md-none">オペレーションサービスの<br class="d-md-none">主な事業</h2>
+				<div class="glay-box">
+					<div class="content">
+						<a href="<?php echo home_url( '/' ); ?>services/management/" class="content-list">
+							<ul class="content-item">
+								<li class="img-content">
+									<div class="img --img_service_management"></div>
+								</li>
+								<li class="text-content">
+									<h3 class="ttl">オペレーションマネジメント事業</h3>
+									<p class="text">空港オペレーション業務全般に係る契約事務・支払い等に関する総合調整事業を行う事務系部門です。関西エアポート（株）から受託する業務に関する予算管理や契約業務の他に、警備・消防・旅客サービスにかかる各種調整を行います。社内外との業務運用調整を行う以外にも、空港内展望ホールの運営も行っています。</p>
+									<div class="btn-bace-round">
+										<div class="btn-setting">詳しく見る</div>
+									</div>
+								</li>
+							</ul>
+						</a>
+					</div>
+				</div>
+				<div class="glay-box">
+					<div class="content">
+						<a href="<?php echo home_url( '/' ); ?>services/security/" class="content-list">
+							<ul class="content-item">
+								<li class="img-content">
+									<div class="img --img_service_security"></div>
+								</li>
+								<li class="text-content">
+									<h3 class="ttl">警備事業</h3>
+									<p class="text">空港内の制限区域・保安区域の出入管理、スクリーニングを行っています。車両巡回や、管制監視業務で、空港の主要部へ不審者の侵入などを未然に防ぐ業務を24時間体制で実施し、空港の安全を守っています。</p>
+									<div class="btn-bace-round">
+										<div class="btn-setting">詳しく見る</div>
+									</div>
+								</li>
+							</ul>
+						</a>
+					</div>
+				</div>
+				<div class="glay-box">
+					<div class="content">
+						<a href="<?php echo home_url( '/' ); ?>services/fire/" class="content-list">
+							<ul class="content-item">
+								<li class="img-content">
+									<div class="img --img_service_firefighting"></div>
+								</li>
+								<li class="text-content">
+									<h3 class="ttl">消防事業</h3>
+									<p class="text">航空機や空港内での事故・災害時には、「人命救助を最優先」とした消火・救難活動を行うことが使命です。日頃から様々な想定で訓練を行い、24時間体制で緊急事態に備えています。また、災害時に備えた災害対応機材の管理・運用、特別訓練の実施などを行っています。滑走路や誘導路において、航空機が安全に離着陸できるために、路面剝離や燃料漏洩、落下物がないか点検する業務である、「飛行場面点検業務」を行っています。</p>
+									<div class="btn-bace-round">
+										<div class="btn-setting">詳しく見る</div>
+									</div>
+								</li>
+							</ul>
+						</a>
+					</div>
+				</div>
+				<div class="glay-box">
+					<div class="content">
+						<a href="<?php echo home_url( '/' ); ?>services/terminal/" class="content-list">
+							<ul class="content-item">
+								<li class="img-content">
+									<div class="img --img_service_terminalmana"></div>
+								</li>
+								<li class="text-content">
+									<h3 class="ttl">ターミナルマネジメント事業</h3>
+									<p class="text">ターミナルビル全体の管理運用、また旅客のニーズに応じたサービスの提供を行っています。空港を利用される国内外からの様々なお客様の対応に加えて、保税手荷物一時預かり業務、KIX-ITMカード業務、IDカード管理業務、防災監視業務、要人対応業務など幅広い業務を担う他、急患発生時の初動対応などの業務も担当しています。また、空港内の旅客・従業員向けの保安検査業務も行っています。</p>
+									<div class="btn-bace-round">
+										<div class="btn-setting">詳しく見る</div>
+									</div>
+								</li>
+							</ul>
+						</a>
+					</div>
+				</div>
+				<div class="glay-box">
+					<div class="content">
+						<a href="<?php echo home_url( '/' ); ?>services/cart-cleaning/" class="content-list">
+							<ul class="content-item">
+								<li class="img-content">
+									<div class="img --img_service_cleaning"></div>
+								</li>
+								<li class="text-content">
+									<h3 class="ttl">清掃・カート事業</h3>
+									<p class="text">ターミナルビル内の清掃及びカードの回収点検を行い、空港を利用されるお客様が快適かつ安全に過ごすことができる環境を提供しています。館内テナントからの清掃依頼の受注、清掃機材・物品の管理に加えて、インスペクション業務も行っています。</p>
+									<div class="btn-bace-round">
+										<div class="btn-setting">詳しく見る</div>
+									</div>
+								</li>
+							</ul>
+						</a>
+					</div>
+				</div>
+				<div class="glay-box">
+					<div class="content">
+						<a href="<?php echo home_url( '/' ); ?>services/cargo/" class="content-list">
+							<ul class="content-item">
+								<li class="img-content">
+									<div class="img --img_service_operation"></div>
+								</li>
+								<li class="text-content">
+									<h3 class="ttl">カーゴエリアオペレーション事業</h3>
+									<p class="text">国際貨物エリアの中での施設管理や連絡窓口としての役割を担っています。また各種災害や重大事案発生時には迅速な情報提供を行うなど、国際貨物エリア内での災害活動拠点として対応を行い、安全確保に努めています。</p>
+									<div class="btn-bace-round">
+										<div class="btn-setting">詳しく見る</div>
+									</div>
+								</li>
+							</ul>
+						</a>
+					</div>
+				</div>
+				<div class="glay-box">
+					<div class="content">
+						<a href="<?php echo home_url( '/' ); ?>services/parking/" class="content-list">
+							<ul class="content-item">
+								<li class="img-content">
+								 <div class="img --img_service_parking"></div>
+								</li>
+								<li class="text-content">
+									<h3 class="ttl">駐車場運営事業</h3>
+									<p class="text">空港内駐車場の運営全般を担っており、出庫・入庫の管理、精算時の補助や車両の監視、予約サービスにも対応しています。その他、不正駐車や不審車両の確認など、駐車場内で発生するイレギュラー事案の対応も行っています。</p>
+									<div class="btn-bace-round">
+										<div class="btn-setting">詳しく見る</div>
+									</div>
+								</li>
+							</ul>
+						</a>
+					</div>
+				</div>
+				<div class="glay-box">
+					<div class="content">
+						<a href="<?php echo home_url( '/' ); ?>services/fueling/" class="content-list">
+							
+							<ul class="content-item">
+								<li class="img-content">
+									<div class="img --img_service_refueling"></div>
+								</li>
+								<li class="text-content">
+									<h3 class="ttl">給油運用事業</h3>
+									<p class="text">関西国際空港の大動脈として、ハイドラントシステムを採用し、24時間絶え間なく飛来する航空機燃料を安全に供給できるよう、施設の運用及び維持管理を行っています。関西空港の給油施設では保管する航空燃料に係る品質等の管理や燃料の受入業務・航空燃料管理業務・運転監視業務・防災業務を行っています。</p>
+									<div class="btn-bace-round">
+										<div class="btn-setting">詳しく見る</div>
+									</div>
+								</li>
+							</ul>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="recruit-bnr-content">
+		<div class="p-side-15-8">
+			<div class="max-128">
+				<!--slide-->
+				<section class="recruit-bnr-content">
+					<a href="<?php echo home_url( '/' ); ?>recruit/" class="recruit-bnr-link">
+						<div class="recruit-bnr-slider-outer">
+							<div class="btn_recruit-bnr-slider-pc">
+								<img src="<?php bloginfo('template_url'); ?>/assets/img/common/btn_recruit-bnr-slider_pc.png" alt="">
+							</div>
+							<div class="btn_recruit-bnr-slider-sp">
+								<img src="<?php bloginfo('template_url'); ?>/assets/img/common/btn_recruit-bnr-slider_sp.png" alt="">
+							</div>
+							
+							
+							<!--/slider-->
+							<ul class="recruit-bnr-slider">
+							<li><img src="<?php bloginfo('template_url'); ?>/assets/img/common/slide_01.png" alt=""></li>
+							<li><img src="<?php bloginfo('template_url'); ?>/assets/img/common/slide_02.png" alt=""></li>
+							<li><img src="<?php bloginfo('template_url'); ?>/assets/img/common/slide_03.png" alt=""></li>
+							<li><img src="<?php bloginfo('template_url'); ?>/assets/img/common/slide_04.png" alt=""></li>
+							<li><img src="<?php bloginfo('template_url'); ?>/assets/img/common/slide_05.png" alt=""></li>
+							<li><img src="<?php bloginfo('template_url'); ?>/assets/img/common/slide_06.png" alt=""></li>
+							</ul>		
+						</div>
+					</a>
+				</section>
+			<!--service-->
+
+			</div>
+		</div>
+	</div>
+	
+	
+	
+	
 
 </main >
 <?php get_template_part( 'inc/footer' ); ?>
