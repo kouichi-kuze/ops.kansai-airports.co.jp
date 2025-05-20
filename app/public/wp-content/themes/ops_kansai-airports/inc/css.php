b05b20e17300661ab209fe6452922f12338915c9
Sayuri Habuka <littlelilly.sky@gmail.com>
2025-05-21 00:12:40 +0900

    header　footer の共通パーツを整理　 ja / cssを共通パーツファイルへ記述

--- app/public/wp-content/themes/ops_kansai-airports/inc/css.php
+++ app/public/wp-content/themes/ops_kansai-airports/inc/css.php
@@ -1,4 +1,6 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700;900&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="/assets/css/style.css">
+<!--<link rel="stylesheet" href="/assets/css/style.css">-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css">
+<link href="<?php echo get_stylesheet_directory_uri() ?>/assets/css/style.css" rel="stylesheet">
--- app/public/wp-content/themes/ops_kansai-airports/inc/script.php
+++ app/public/wp-content/themes/ops_kansai-airports/inc/script.php
@@ -1,1 +1,4 @@
+<!-- jQuery CDN --> 
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/assets/js/common.js"></script>
