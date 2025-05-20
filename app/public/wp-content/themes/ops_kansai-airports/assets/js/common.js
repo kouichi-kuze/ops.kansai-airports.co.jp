a7881118ae41d8a724eaede18d6ff4ca97cdebe8
Sayuri Habuka <littlelilly.sky@gmail.com>
2025-05-21 00:14:29 +0900

    js追加　お知らせTOPのタブ切り替え

--- app/public/wp-content/themes/ops_kansai-airports/assets/js/common.js
+++ app/public/wp-content/themes/ops_kansai-airports/assets/js/common.js
@@ -122,6 +122,49 @@ document.addEventListener('DOMContentLoaded', () => {
 	]
 	});
 	
+	//任意のタブにURLからリンクするための設定
+function GethashID (hashIDName){
+	if(hashIDName){
+		//タブ設定
+		$('.information_list__tags li').find('p').each(function() { //タブ内のaタグ全てを取得
+			var idName = $(this).attr('id'); //タブ内のaタグのリンク名（例）#lunchの値を取得	
+			if(idName == hashIDName){ //リンク元の指定されたURLのハッシュタグ（例）http://example.com/#lunch←この#の値とタブ内のリンク名（例）#lunchが同じかをチェック
+				var parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
+				$('.information_list__tags li').removeClass("active"); //タブ内のliについているactiveクラスを取り除き
+				$(parentElm).addClass("active"); //リンク元の指定されたURLのハッシュタグとタブ内のリンク名が同じであれば、liにactiveクラスを追加
+				//表示させるエリア設定
+				$(".tab-area").removeClass("is-active"); //もともとついているis-activeクラスを取り除き
+				$(hashIDName).addClass("is-active"); //表示させたいエリアのタブリンク名をクリックしたら、表示エリアにis-activeクラスを追加	
+			}
+		});
+	}
+}
+
+//タブをクリックしたら
+$('.information_list__tags p').on('click', function() {
+	var idName = $(this).attr('id'); //タブ内のリンク名を取得	
+	GethashID (idName);//設定したタブの読み込みと
+	return false;//aタグを無効にする
+});
+
+
+// 上記の動きをページが読み込まれたらすぐに動かす
+$(window).on('load', function () {
+    $('.information_list__tags li:first-of-type').addClass("active"); //最初のliにactiveクラスを追加
+    $('.tab-area:first-of-type').addClass("is-active"); //最初の.tab-area-activeクラスを追加
+	var hashName = location.hash; //リンク元の指定されたURLのハッシュタグを取得
+	GethashID (hashName);//設定したタブの読み込み
+});
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 });
