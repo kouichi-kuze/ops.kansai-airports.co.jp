0ca00169b23c8827841ebd8bda7745540f27109f
K.Kuze（久世） <info@kuzeya.com>
2025-05-28 08:20:10 +0900

    JSのCDNをローカルに変更

--- app/public/wp-content/themes/ops_kansai-airports/inc/script.php
+++ app/public/wp-content/themes/ops_kansai-airports/inc/script.php
@@ -1,5 +1,7 @@
 <!-- jQuery CDN --> 
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/vegas/2.5.1/vegas.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/assets/js/jquery-3.7.1.min.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/assets/js/vegas.min.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/assets/js/slick.min.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/assets/js/gsap.min.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/assets/js/ScrollTrigger.min.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/assets/js/common.js"></script>
