08baaaf9e2ac3eded026b0a7d728e594d1c9634d
K.Kuze（久世） <info@kuzeya.com>
2025-05-28 09:11:58 +0900

    CSSのCDNをローカルに変更

--- app/public/wp-content/themes/ops_kansai-airports/inc/css.php
+++ app/public/wp-content/themes/ops_kansai-airports/inc/css.php
@@ -2,6 +2,6 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700;900&display=swap" rel="stylesheet">
 <!--<link rel="stylesheet" href="/assets/css/style.css">-->
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/vegas/2.4.4/vegas.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css">
+<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri() ?>/assets/css/assets/css/vegas.min.css">
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/assets/css/slick.min.css">
 <link href="<?php echo get_stylesheet_directory_uri() ?>/assets/css/style.css" rel="stylesheet">
